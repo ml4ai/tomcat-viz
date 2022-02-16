868789b4661b0e073d0ff439ac2776901340a3a3
Paulo Soares <paulosoares@SBS-7186.local>
2022-02-15 17:32:03 -0700

    Better colors

--- imap/Gui/CustomScene.py
+++ imap/Gui/CustomScene.py
@@ -1,4 +1,4 @@
-from PyQt5.Qt import QGraphicsScene, Qt, QPen, QBrush, QFont, QGraphicsItem
+from PyQt5.Qt import QGraphicsScene, Qt, QPen, QBrush, QFont, QGraphicsItem, QColor
 
 from imap.Gui.StampedRectItem import StampedRectItem
 from imap.Parser.MarkerType import MarkerType
@@ -13,13 +13,13 @@ class CustomScene(QGraphicsScene):
         return self._drawBlock(x, y, scale, blockSize, Qt.gray, Qt.lightGray)
 
     def drawRed(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawCircle(x, y, scale, blockSize, Qt.red, Qt.red)
+        return self._drawCircle(x, y, scale, blockSize, QColor("#E30B21"), QColor("#E30B21"))
 
     def drawGreen(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawCircle(x, y, scale, blockSize, Qt.green, Qt.green)
+        return self._drawCircle(x, y, scale, blockSize, QColor("#13F000"), QColor("#13F000"))
 
     def drawBlue(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawCircle(x, y, scale, blockSize, Qt.blue, Qt.blue)
+        return self._drawCircle(x, y, scale, blockSize, QColor("#0045FA"), QColor("#0045FA"))
 
     def drawVictimSignalBlock(self, x: float, y: float, scale: float, blockSize: float):
         return self._drawStampedBlock("V", x, y, scale, blockSize, Qt.white, Qt.black, Qt.black)
@@ -28,16 +28,20 @@ class CustomScene(QGraphicsScene):
         return self._drawBlock(x, y, scale, blockSize, Qt.darkGray, Qt.darkGray, Qt.Dense5Pattern)
 
     def drawVictimA(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawStampedBlock("A", x, y, scale, blockSize, Qt.darkGreen, Qt.darkGreen, Qt.white)
+        return self._drawStampedBlock("A", x, y, scale, blockSize, QColor("#02B860"), QColor("#02B860"), Qt.black)
 
     def drawVictimB(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawStampedBlock("B", x, y, scale, blockSize, Qt.darkGreen, Qt.darkGreen, Qt.white)
+        return self._drawStampedBlock("B", x, y, scale, blockSize, QColor("#02B860"), QColor("#02B860"), Qt.black)
 
     def drawCriticalVictim(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawBlock(x, y, scale, blockSize, Qt.darkYellow, Qt.darkYellow)
+        # return self._drawBlock(x, y, scale, blockSize, QColor("#FACC09"), QColor("#FACC09"))
+        return self._drawStampedBlock("C", x, y, scale, blockSize, QColor("#FACC09"), QColor("#FACC09"), Qt.black)
+
+    def drawSafeVictimA(self, x: float, y: float, scale: float, blockSize: float):
+        return self._drawStampedBlock("A", x, y, scale, blockSize, QColor("#0AB7FC"), QColor("#0AB7FC"), Qt.black)
 
     def drawRubbleCollapseBlock(self, x: float, y: float, scale: float, blockSize: float):
-        return self._drawBlock(x, y, scale, blockSize, Qt.red, Qt.red)
+        return self._drawBlock(x, y, scale, blockSize, QColor("#8400EB"), QColor("#8400EB"))
 
     def drawDoor(self, x: float, y: float, scale: float, blockSize: float):
         return self._drawBlock(x, y, scale, blockSize, Qt.black, Qt.white, Qt.Dense7Pattern)
@@ -62,7 +66,7 @@ class CustomScene(QGraphicsScene):
         elif type == MarkerType.SOS:
             label = "S"
 
-        return self._drawStampedBlock(label, x, y, scale, blockSize, Qt.black, Qt.black, Qt.white)
+        return self._drawStampedBlock(label, x, y, scale, blockSize, QColor("#E602D5"), QColor("#E602D5"), Qt.black)
 
     def addItem(self, item: QGraphicsItem) -> None:
         if isinstance(item, StampedRectItem):
@@ -82,18 +86,18 @@ class CustomScene(QGraphicsScene):
                           borderColor: Qt, textColor: Qt):
         pen = QPen(borderColor, 1, Qt.SolidLine)
         brush = QBrush(color, Qt.SolidPattern)
-        rectItem = self.addRect(x * scale, y * scale, blockSize, blockSize, pen, brush)
+        rectItem = self.addRect(x * scale + 1, y * scale + 1, blockSize - 1, blockSize - 1, pen, brush)
 
-        textItem = self.addText(text, QFont('Arial', 110 / blockSize))
+        textItem = self.addText(text, QFont('Arial', 100 / blockSize))
         textItem.setDefaultTextColor(textColor)
-        textItem.setPos((x - 2 / blockSize) * scale, (y - 5.5 / blockSize) * scale)
+        textItem.setPos((x - 2 / blockSize) * scale, (y - 4.5 / blockSize) * scale)
         return StampedRectItem(rectItem, textItem)
 
     def _drawBlock(self, x: float, y: float, scale: float, block_size: float, color: Qt,
                    borderColor: Qt, brushPattern: Qt = Qt.SolidPattern) -> QGraphicsItem:
         pen = QPen(borderColor, 1, Qt.SolidLine)
         brush = QBrush(color, brushPattern)
-        return self.addRect(x * scale, y * scale, block_size, block_size, pen, brush)
+        return self.addRect(x * scale + 1, y * scale + 1, block_size - 1, block_size - 1, pen, brush)
 
     def _drawCircle(self, x: float, y: float, scale: float, diameter: float, color: Qt,
                     borderColor: Qt) -> QGraphicsItem:
